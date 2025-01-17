3c68bd02c7320cd46e9ee33eb4854e374f228ace
Elhoucine Elmourabit <elhoucine.elmourabit.pro@gmail.com>
2025-01-17 19:10:37 +0100

    feat(db): ajout de la connexion à MongoDB et de la fonction listDatabases
    - Ajout de la logique de connexion à MongoDB pour établir la connectivité avec le cluster.
    - Création de la fonction listDatabases pour afficher les bases de données disponibles dans le cluster.
    - Permet de vérifier que la connexion au cluster est réussie.

--- src/config/db.js
+++ src/config/db.js
@@ -9,9 +9,33 @@ const config = require('./env');
 
 let mongoClient, redisClient, db;
 
+
 async function connectMongo() {
   // TODO: Implémenter la connexion MongoDB
   // Gérer les erreurs et les retries
+  mongoClient = new MongoClient(config.mongodb.uri);
+  
+  try{
+    await mongoClient.connect();
+
+    await listDatabases(mongoClient);
+  } catch(e){
+    console.error(e);
+  } finally{
+    await mongoClient.close();
+  }
+
+}
+
+connectMongo().catch(console.error);
+
+async function listDatabases(mongoClient){
+  const databasesList = await mongoClient.db().admin().listDatabases();
+
+  console.log("Databases: ");
+  databasesList.databases.forEach(db => {
+    console.log(` ${db.name}`);
+  });
 }
 
 async function connectRedis() {
@@ -22,4 +46,5 @@ async function connectRedis() {
 // Export des fonctions et clients
 module.exports = {
   // TODO: Exporter les clients et fonctions utiles
+  connectMongo
 };
